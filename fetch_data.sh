cb2b2ee4a607ecf128c3fe1cefc8d06b2a83a4be
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-02-27 09:00:06 +0100

    First commit

---
+++ fetch_data.sh
@@ -0,0 +1,17 @@
+# bin/bash
+
+echo "fetching transkriptions from data_repo"
+rm -rf data
+mkdir -p data/indices
+mkdir data/meta
+curl -LO https://github.com/ofmgraz/transkribus-out/archive/refs/heads/main.zip
+unzip main
+mv ./transkribus-out-main/data/* ./data
+
+
+curl -LO https://github.com/ofmgraz/ofm-para-text/archive/refs/heads/main.zip
+unzip main
+mv ./ofm-para-text-main/data/meta ./data/
+
+rm main.zip
+rm -rf ./transkribus-out-main ./ofm-para-text-main
